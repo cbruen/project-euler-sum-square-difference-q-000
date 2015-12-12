c674e7ccc65898aab6a7582e462d5c0ca7fc451b
cbruen <bruen.chris@gmail.com>
2015-12-12 06:01:38 +0000

    Done.

--- lib/oo_sum_square_difference.rb
+++ lib/oo_sum_square_difference.rb
@@ -1,1 +1,17 @@
 # Implement your object-oriented solution here!
+class SumSquareDifference
+  def initialize(limit)
+    @limit = limit
+  end
+
+  def difference
+  series = (1..@limit).to_a
+  one = series.map { |x| x**2}
+  one = one.inject(:+)
+  two = series.inject(:+)
+  two = two **2
+  final = two - one
+  return final
+  end
+
+end
--- lib/sum_square_difference.rb
+++ lib/sum_square_difference.rb
@@ -1,1 +1,16 @@
 # Implement your procedural solution here!
+def sum_square_difference(limit)
+
+series = (1..limit).to_a
+
+one = series.map { |x| x**2}
+one = one.inject(:+)
+
+two = series.inject(:+)
+two = two **2
+
+final = two - one
+return final
+
+end
+
